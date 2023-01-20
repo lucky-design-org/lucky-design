85044673448ecd1827ab4b88d0cc0a3e0ee4de0a
付志强 <zhiqiangfu6@gmail.com>
2023-01-20 21:09:39 +0800

    feat(components): add vitest and button struct

---
+++ packages/components/button/__tests__/button.test.tsx
@@ -0,0 +1,6 @@
+import { describe, expect, test } from 'vitest'
+describe('Alert.vue', () => {
+  test('hello world', () => {
+    expect(1).toBe(1)
+  })
+})
---
+++ packages/components/button/index.ts
@@ -0,0 +1,5 @@
+import Button from './src/button.vue'
+
+export default Button
+
+export * from './src/button'
