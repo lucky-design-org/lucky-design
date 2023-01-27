140a39d4d318401c8c8903967f3ceddc6195ff56
snowingfox <1503401882@qq.com>
2023-01-27 14:54:10 +0800

    fix: fix type check err

--- packages/components/unocss.config.ts
+++ packages/components/unocss.config.ts
@@ -1,5 +1,5 @@
 import { presetAttributify, presetUno } from 'unocss'
-import { defineConfig } from '@unocss/vite'
+import { defineConfig } from 'unocss/vite'
 import { presetLuckyDesign } from '@lucky-design/preset'
 
 export default defineConfig({
