8059eab77905ac30c971e20e7d1a40174a0811d7
snowingfox <1503401882@qq.com>
2023-01-31 18:48:13 +0800

    feat: support rebuild package with chokidar

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -3,7 +3,12 @@ import { logger, run } from './utils'
 
 async function dev() {
   try {
-    run('vue-tsc', ['--declaration', '--emitDeclarationOnly', '--skipLibCheck', '--watch'])
+    run('vue-tsc', [
+      '--declaration',
+      '--emitDeclarationOnly',
+      '--skipLibCheck',
+      '--watch',
+    ])
     run('vite', ['build', '--watch'])
     logger.success(`${process.cwd()}: are running in dev mode.`)
   }
