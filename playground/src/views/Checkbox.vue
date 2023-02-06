e2ae44201b90d5a23ee4cac4cd19b2d17b8b0d3f
付志强 <zhiqiangfu6@gmail.com>
2023-02-06 20:19:55 +0800

    feat(packages): add scripts and checkbox playground

---
+++ playground/src/views/Checkbox.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { LCheckbox } from 'lucky-design'
+const handleInput = (value: boolean) => {
+  // console.log('[ value ] >', value)
+}
+</script>
+
+<template>
+  <div>
+    <div class="container">
+      <LCheckbox :value="true" @input="handleInput"> 北京 </LCheckbox>
+      <LCheckbox :value="false" :disabled="true"> 上海 </LCheckbox>
+      <LCheckbox :true-value="true"> 广州 </LCheckbox>
+      <LCheckbox :false-value="false"> 杭州 </LCheckbox>
+    </div>
+  </div>
+</template>
