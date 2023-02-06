2ef9952825b1e1dd44e442daf6d1125106137ac0
付志强 <zhiqiangfu6@gmail.com>
2023-02-06 17:48:42 +0800

    feat: add basis checkbox

--- packages/components/src/checkbox/checkbox.vue
+++ packages/components/src/checkbox/checkbox.vue
@@ -1,11 +1,38 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { checkboxProps } from './props'
+const { value, label, disabled, trueValue, falseValue }
+  = defineProps(checkboxProps)
+const emit = defineEmits(['input'])
+const currentValue = ref(value)
+
+const change = (event: any) => {
+  if (disabled)
+    return
+
+  const checked = event.target.checked
+  currentValue.value = checked
+  const value = checked ? trueValue : falseValue
+  emit('input', value)
+}
 </script>
 
 <template>
-  <h1>demo</h1>
-  <button>checkbox</button>
+  <label>
+    <input
+      type="checkbox"
+      :checked="currentValue"
+      :disabled="disabled"
+      :class="{ disabled }"
+      @change="change"
+    >
+    <slot />
+  </label>
 </template>
 
 <style scoped>
-
+/* TODO:scss抽离 */
+.disabled {
+  cursor: not-allowed;
+}
 </style>
