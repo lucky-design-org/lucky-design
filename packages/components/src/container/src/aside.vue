88fa8801f40097cfe56a4077405bbd9033212ab3
付志强 <zhiqiangfu6@gmail.com>
2023-02-20 16:41:03 +0800

    feat: merge

--- packages/components/src/container/src/aside.vue
+++ packages/components/src/container/src/aside.vue
@@ -1,16 +1,17 @@
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+import type { CSSProperties } from 'vue'
+import { asideProps } from '../props'
+
+const style = computed(
+  () =>
+    (asideProps.width ? { width: asideProps.width } : {}) as CSSProperties,
+)
+</script>
+
 <template>
   <aside class="ld-aside" :style="style">
     <slot />
   </aside>
 </template>
-<script lang="ts" setup>
-import { computed } from 'vue'
-
-import type { CSSProperties } from 'vue'
-import {asideProps} from '../props'
-
-const style = computed(
-  () =>
-    (asideProps.width ? { width: asideProps.width } : {}) as CSSProperties
-)
-</script>
